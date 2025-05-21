e9021e1a0c6648e55b11430c64c6c83be353f320
Jossiah Kibona <jossiahoscar@gmail.com>
2025-05-21 15:43:22 +0300

    merged with dev

--- src/main/java/com/tanre/document_register/service/EvidenceService.java
+++ src/main/java/com/tanre/document_register/service/EvidenceService.java
@@ -2,12 +2,15 @@ package com.tanre.document_register.service;
 
 import com.tanre.document_register.dto.EvidenceDTO;
 import com.tanre.document_register.model.Document;
+import com.tanre.document_register.model.DocumentTransaction;
 import com.tanre.document_register.model.Evidence;
 import com.tanre.document_register.model.Status;
 import com.tanre.document_register.repository.DocumentRepository;
+import com.tanre.document_register.repository.DocumentTransactionRepository;
 import com.tanre.document_register.repository.EvidenceRepository;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,10 +23,12 @@ import java.util.stream.Collectors;
 public class EvidenceService {
     private final EvidenceRepository evidenceRepo;
     private final DocumentRepository docRepo;
+    private final DocumentTransactionRepository documentTransactionRepository;
 
-    public EvidenceService(EvidenceRepository evidenceRepo, DocumentRepository docRepo) {
+    public EvidenceService(EvidenceRepository evidenceRepo, DocumentRepository docRepo, DocumentTransactionRepository documentTransactionRepository) {
         this.evidenceRepo = evidenceRepo;
         this.docRepo = docRepo;
+        this.documentTransactionRepository = documentTransactionRepository;
     }
 
     public Evidence addEvidence(Long documentId, MultipartFile file) throws IOException {
@@ -36,6 +41,15 @@ public class EvidenceService {
         e.setContentType(file.getContentType());
         e.setData(file.getBytes());
         Evidence saved = evidenceRepo.save(e);
+
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        DocumentTransaction documentTransaction = new DocumentTransaction();
+        documentTransaction.setDocument(doc);
+        documentTransaction.setOldStatus(doc.getStatus());
+        documentTransaction.setNewStatus(Status.DONE);
+        documentTransaction.setComment("Evidence added");
+        documentTransaction.setChangedBy(user);
+        documentTransactionRepository.save(documentTransaction);
 
         doc.setStatus(Status.DONE);
         docRepo.save(doc);
